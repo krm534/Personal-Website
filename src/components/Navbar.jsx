a7d9ae699c7da978fc308d31e05e1dee7ade1bd0
Kaleb Meeks <krmeeks15@gmail.com>
2022-02-21 18:25:35 -0600

    Updated navbar to look better in desktop environment

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Link } from 'react-scroll';
+import '../styles/Navbar.css';
+
+function Navbar() {
+  const [dialogBoxShowing, setDialogBoxShowing] = useState(false);
+
+  return (
+    <div className="navbar-container">
+      <div className="navbar-hamburger-button-container" style={{ display: dialogBoxShowing ? 'none' : 'block' }}>
+        <i className="fa fa-bars" aria-hidden="true" onClick={() => setDialogBoxShowing(true)} role="button" />
+      </div>
+      <div className="navbar-content-container" style={{ display: dialogBoxShowing ? 'block' : 'none' }}>
+        <div className="navbar-exit-button-container">
+          <i className="fa fa-close" aria-hidden="true" onClick={() => setDialogBoxShowing(false)} />
+        </div>
+        <ul>
+          <Link activeClass="active" className="navbar-link" to="home" spy smooth>
+            <li>Home</li>
+          </Link>
+          <Link className="navbar-link" to="about" spy smooth>
+            <li>About</li>
+          </Link>
+          <Link className="navbar-link" to="projects" spy smooth>
+            <li>Projects</li>
+          </Link>
+          <Link className="navbar-link" to="contact" spy smooth>
+            <li>Contact</li>
+          </Link>
+        </ul>
+        <div className="navbar-contact-section">
+          <hr />
+          <div className="navbar-contact-image-container">
+            <div>
+              <a
+                href="https://www.linkedin.com/in/kaleb-meeks-060082188/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="/images/contact-images/linkedin-logo.png" alt="Linkedin company logo" />
+              </a>
+            </div>
+            <div>
+              <a
+                href="https://github.com/krm534"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="/images/contact-images/github-logo.png" alt="Github company logo" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;

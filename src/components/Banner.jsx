3b5f20e297d25cf98786a9827c1b6b4801565a38
Kaleb Meeks <krmeeks15@gmail.com>
2022-03-28 15:49:08 -0500

    Fixed main header tag and updated website title to something more fitting

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,7 +9,7 @@ function Banner() {
         <div className="background-cover" />
         <div className="banner-details">
           <h1>Kaleb Meeks</h1>
-          <h2>Software Engineer</h2>
+          <h1>Software Engineer</h1>
         </div>
         <div className="scroll-button-container">
           <Link
